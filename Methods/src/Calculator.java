dc5b898379cb57d126c105e789a552893ca759e4
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-06 10:39:54 +0300

    Adding simple (add)Calculator using method

---
+++ Methods/src/Calculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public Integer add (Integer arg1, Integer arg2) {
+
+        Integer result = arg1 + arg2;
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+        Integer result = calculator.add(num1, num2);
+        System.out.println("Result: " + result);
+    }
+}
+
