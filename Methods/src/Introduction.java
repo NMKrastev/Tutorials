ca2b09c5096ca3432bc158327a290842c7f34b9c
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-06 16:08:42 +0300

    Created two methods that are called from Main

---
+++ Methods/src/Introduction.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Introduction {
+
+    public static void name() {
+        System.out.println("Nikola Krastev");
+    }
+
+    public static void age () {
+        System.out.println(32);
+    }
+}
