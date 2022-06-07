9827447bba8f0d8551ff998a8a7237851406731a
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-07 12:52:11 +0300

    Override toString() method in Car class so it can return string representation of an object

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Uses Car class
-        /*Car myFirstCar = new Car();
-        Car mySecondCar = new Car();
+        Car myFirstCar = new Car();
+        /*Car mySecondCar = new Car();
         System.out.println(myFirstCar.make);
         System.out.println(myFirstCar.model);
         System.out.println();
@@ -14,6 +14,8 @@ public class Main {
 
         myFirstCar.drive();
         myFirstCar.brake();*/
+        //System.out.println(myFirstCar.toString());
+        System.out.println(myFirstCar);
 
         //Uses Human class
         /*Human humanOne = new Human("Nikola", 32, 105.5);
@@ -25,12 +27,12 @@ public class Main {
         humanOne.drink();*/
 
         //Uses Pizza class
-        Pizza pizza = new Pizza("thick crust", "tomato", "mozzarella", "pepperoni");
+        /*Pizza pizza = new Pizza("thick crust", "tomato", "mozzarella", "pepperoni");
         System.out.println("Here are the ingredients of your pizza:");
         System.out.println(pizza.bread);
         System.out.println(pizza.sauce);
         System.out.println(pizza.cheese);
-        System.out.println(pizza.topping);
+        System.out.println(pizza.topping);*/
 
     }
 }
