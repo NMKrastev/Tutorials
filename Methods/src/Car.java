d3a32ab1d1b51a816444fe6d645f7c65ed840e1f
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-07 12:09:41 +0300

    Created new Class that uses methods

---
+++ Methods/src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+
+    String make = "Nissan";
+    String model = "Skyline R-34";
+    int year = 2000;
+    String color = "Red";
+    double price = 35000.00;
+
+    void drive() {
+        System.out.println("You drive the car!");
+    }
+    void brake() {
+        System.out.println("You step on the brakes!");
+    }
+}
--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -2,5 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Car myFirstCar = new Car();
+        Car mySecondCar = new Car();
+        System.out.println(myFirstCar.make);
+        System.out.println(myFirstCar.model);
+        System.out.println();
+        System.out.println(mySecondCar.make);
+        System.out.println(mySecondCar.model);
+
+        myFirstCar.drive();
+        myFirstCar.brake();
     }
 }
