b40aa63a6416b25253df15c0b49857a6841fe4ff
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-29 17:36:23 +0300

    Added toString method

---
+++ 05-toStringMethod/src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+
+    String make = "Mercedes";
+    String model = "GLC-Class Coupe";
+    String color = "Black";
+    int year = 2022;
+
+    public String toString() {
+        //String car = make + "\n" + model + "\n" + color + "\n" + year + "\n";
+        return make + "\n" + model + "\n" + color + "\n" + year + "\n";
+    }
+}
---
+++ 05-toStringMethod/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //toString() - special method that all objects inherit,
+        //that returns a string that "textually represents" an object.
+        //Can be used both implicitly and explicitly.
+
+        Car car = new Car();
+        //explicitly
+        //System.out.println(car.toString());
+        //implicitly
+        System.out.println(car);
+
+    }
+}
