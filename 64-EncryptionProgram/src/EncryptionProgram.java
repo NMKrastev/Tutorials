6275d2defa56a6219368007e90aedc3e1207f8ae
Nikola <nikola.malinov.krastev@gmail.com>
2022-07-11 13:14:40 +0300

    Created variables

--- 64-EncryptionProgram/src/EncryptionProgram.java
+++ 64-EncryptionProgram/src/EncryptionProgram.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class EncryptionProgram {
+
+    private Scanner scanner;
+    private Random random;
+    private ArrayList<Character> list;
+    private ArrayList<Character> shuffledList;
+    private char character;
+    private String line;
+    private char[] letters;
+    private char[] secretLetters;
 
     EncryptionProgram() {
 
