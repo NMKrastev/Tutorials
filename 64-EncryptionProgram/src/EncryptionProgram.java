51c9dfc37b82a7df9d4d957461000b3a9163dd61
Nikola <nikola.malinov.krastev@gmail.com>
2022-07-11 13:22:53 +0300

    Created the constructor and askQuestion method

--- 64-EncryptionProgram/src/EncryptionProgram.java
+++ 64-EncryptionProgram/src/EncryptionProgram.java
@@ -15,10 +15,47 @@ public class EncryptionProgram {
 
     EncryptionProgram() {
 
+        scanner = new Scanner(System.in);
+        random = new Random();
+        list = new ArrayList<>();
+        shuffledList = new ArrayList<>();
+        character = ' ';
+
+        newKey();
+        askQuestion();
+
     }
 
     private  void askQuestion() {
 
+        while (true) {
+
+            System.out.println("------------------------------------------------");
+            System.out.println("What do you want to do?");
+            System.out.println("(N)ewKey, (G)etKey, (E)ncrypt, (D)ecrypt, (Q)uit");
+            char response = Character.toUpperCase(scanner.nextLine().charAt(0));
+
+            switch (response) {
+
+                case 'N' :
+                    newKey();
+                    break;
+                case 'G' :
+                    getKey();
+                    break;
+                case 'E' :
+                    encrypt();
+                    break;
+                case 'D' :
+                    decrypt();
+                    break;
+                case 'Q' :
+                    quit();
+                    break;
+                default:
+                    System.out.println("Not a valid answer!");
+            }
+        }
     }
 
     private void newKey() {
