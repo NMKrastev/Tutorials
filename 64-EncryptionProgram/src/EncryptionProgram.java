9fa1c191fe794c38c8913a693968e4abbe14d724
Nikola <nikola.malinov.krastev@gmail.com>
2022-07-11 13:07:37 +0300

    Initial commit

---
+++ 64-EncryptionProgram/src/EncryptionProgram.java
@@ -0,0 +1,6 @@
+public class EncryptionProgram {
+
+    EncryptionProgram() {
+
+    }
+}
---
+++ 64-EncryptionProgram/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Substitution cipher
+
+        EncryptionProgram ep = new EncryptionProgram();
+    }
+}
