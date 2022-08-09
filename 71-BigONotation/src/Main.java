df643faff6b4c136c675b1ed69f0a31ff9658cc0
Nikola <nikola.malinov.krastev@gmail.com>
2022-08-09 10:10:16 +0300

    Added Big O notation

---
+++ 71-BigONotation/src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*      Big O Notation
+        * "How code slows as data grows"
+        * 1. Describes the performance of an algorithm as
+        * the amount of data increases
+        * 2. Machine independent (number ot steps to completion)
+        * 3. Ignore smaller operations 0(n+1) -> 0(n)*/
+
+        /* Example:
+        * 0(1)             n = amount of data
+        * 0(n)             (it's a variable like x
+        * 0(log n)
+        * 0(n^2)*/
+
+        /* 0(1) - constant time
+        * 1. random access of an element in an array
+        * 2. inserting at the beginning of LinkedLists
+        *
+        * 0(log n) - logarithmic time
+        * 1. binary search
+        *
+        * 0(n) - linear time
+        * 1. looping through elements in an array
+        * 2. searching through a LinkedList
+        *
+        * 0(n log n) - quasilinear time
+        * 1. quicksort
+        * 2. mergesort
+        * 3. heapsort
+        *
+        * 0(n^2) - quadratic time
+        * 1. insertion sort
+        * 2. selection sort
+        * 3. bubblesort
+        *
+        * 0(n!) - factorial time
+        * **Traveling salesman problem** */
+    }
+}
